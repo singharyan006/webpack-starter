2c750c90be446f7aaa48e8fe3b2c39ea659ebb87
singharyan006 <aryanraisingh2006@gmail.com>
2025-07-09 23:20:37 +0530

    Intial commit - Webpack template

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+const root = document.createElement("div");
+root.textContent = "Hello, Webpack Template!";
+document.body.appendChild(root);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // clean old builds
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    open: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
